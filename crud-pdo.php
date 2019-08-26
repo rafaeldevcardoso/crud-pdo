a8f035ea4de96b98aafb19d2a361f0383174bb6e
Rafael Cardoso <rafadev_rj@hotmail.com>
2019-08-25 23:30:48 -0300

    adicionado prepare e query

--- crud-pdo.php
+++ crud-pdo.php
@@ -2,7 +2,7 @@
 //--------------------------CONEXAO--------------------------
 try {
                     //tipo do SQL, nome do banco, usuario e senha
-$var_pdo = new PDO("mysql:dbname=CRUDPDO;host=localhost","root","");
+$var_pdo = new PDO("mysql:dbname=crudpdo;host=localhost","root","");//ligação com o banco
 
 } 
 catch (PDOException $e) {
@@ -13,6 +13,16 @@ catch (Exception $e){
 }
 
 //--------------------------INSERT--------------------------
+//1 forma: é a mais utilizada.
+//                    id é auto increment então não precisa ser posto
+//$res = $var_pdo->prepare("INSERT INTO pessoa(nome, telefone, email) VALUES (:nom, :tel, :mail)");
+//$res->bindValue(":nom","Rafael");
+//$res->bindValue(":tel","99999999");
+//$res->bindValue(":mail","teste@gmail.com");
+//$res->execute();
+
+//2 forma:
+$var_pdo->query("INSERT INTO pessoa(nome, telefone, email) VALUES ('Zeca','51515151','zeca@gmail.com')");
 
 
 
